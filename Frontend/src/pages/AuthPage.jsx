d5ea617cddba3a0c18811de9e70765465732418b
Subhankito Roy Choudhury <subhankitoroychoudhuryofficial@gmail.com>
2026-05-03 00:50:04 +0530

    change url for deployement

--- Frontend/src/pages/AuthPage.jsx
+++ Frontend/src/pages/AuthPage.jsx
@@ -68,7 +68,8 @@ const AuthPage = () => {
   };
 
   const handleGoogleLogin = () => {
-    window.location.href = 'http://localhost:8080/oauth2/authorization/google';
+   const baseUrl = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080';
+  window.location.href = `${baseUrl}/oauth2/authorization/google`;
   };
 
   const shakeVariants = {
